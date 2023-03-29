3f563ce49429fe7772be31de6112b575bf29a337
kirankaranth1@gmail.com <kirankaranth1@gmail.com>
2023-03-29 12:41:50 +0000

    a❇️ New     - Pipeline        - P1        - P2

---
+++ pipelines/P1/code/p1/graph/orders.py
@@ -0,0 +1,18 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from p1.config.ConfigStore import *
+from p1.udfs.UDFs import *
+
+def orders(spark: SparkSession) -> DataFrame:
+    return spark.read\
+        .schema(
+          StructType([
+            StructField("order_id", IntegerType(), True), StructField("customer_id", IntegerType(), True), StructField("order_status", StringType(), True), StructField("order_category", StringType(), True), StructField("order_date", StringType(), True), StructField("amount", DoubleType(), True)
+        ])
+        )\
+        .option("header", True)\
+        .option("sep", ",")\
+        .option("ignoreLeadingWhiteSpace", True)\
+        .option("ignoreTrailingWhiteSpace", True)\
+        .csv("dbfs:/Prophecy/kiranadmin@prophecy.io/OrdersDatasetInput.csv")
